745f3140a0e21c4ae2327c5744f6e2faf6425d85
lehongtrang272 <lehongtrang1992@gmail.com>
2018-05-23 23:11:35 +0300

    training list template started

---
+++ src/TrainingList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import "react-table/react-table.css"
+
+class TrainingList extends Component {
+    state = { training}
+}
